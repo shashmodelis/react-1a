3aeb965d61c1e3f8a8185c0eb8b35fb084265e2a
Shashvat Sinha <shashvat@shashvat-mbp.fios-router.home>
2019-04-05 02:04:04 -0400

    class to className

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Dashboard = () => {
     return(
-        <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Work in Progress</h1>
-            <p class="lead">This dashboard is on its way...</p>
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">Work in Progress</h1>
+            <p className="lead">This dashboard is on its way...</p>
         </div>
         </div>
     );
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Login = () => {
     return(
-        <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Work in Progress</h1>
-            <p class="lead">The login screen is on its way...</p>
+        <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+            <h1 className="display-4">Work in Progress</h1>
+            <p className="lead">The login screen is on its way...</p>
         </div>
         </div>
     );
