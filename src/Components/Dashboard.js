24df9825235b11e7283607d3e6cc5c4d051290bd
Shashvat Sinha <shashvat@shashvat-mbp.local>
2019-05-05 15:21:15 -0400

    Revert "Started with Victory Charts"
    This reverts commit b0c1e199af61db1684ec0e518aeefe9c1543f6bf.

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,66 +2,14 @@ import React, {Component} from 'react';
 
 import axios from 'axios';
 
-import BarChart from './Charts/BarChart'
-import PieChart from './Charts/PieChart'
-import LineChart from './Charts/LineChart'
-import AreaChart from './Charts/AreaChart'
-
 class Dashboard extends Component {
     render() {
         return(
         <div className="jumbotron jumbotron-fluid">
         <div className="container">
-            <p className="lead">Supercharger Dashboard</p>
+            <h1 className="display-4">Work in Progress</h1>
+            <p className="lead">The dashboard is on its way...</p>
         </div>
-        <div className="container">        
-            <div className="row" >
-                <div className="col-lg-3 col-md-12">
-                    <div className="card border-primary mb-3" >
-                        <div className="card-body">
-                            <h4 className="card-title">Victory Bar</h4>
-                            <div className="card-text">
-                                <BarChart />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-12">
-                    <div className="card border-primary mb-3" >
-                        <div className="card-body">
-                            <h4 className="card-title">Victory Pie</h4>
-                            <div className="card-text">
-                                <PieChart />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-12">
-                    <div className="card border-primary mb-3" >
-                        <div className="card-body">
-                            <h4 className="card-title">Victory Line</h4>
-                            <div className="card-text">
-                                <LineChart />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-12">
-                    <div className="card border-primary mb-3" >
-                        <div className="card-body">
-                            <h4 className="card-title">Victory Area</h4>
-                            <div className="card-text">
-                                <AreaChart />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-
-
         </div>
     );
     }
